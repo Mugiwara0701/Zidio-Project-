ad92027fa3bb62970676247f2cd29d7dc4989962
shivtf <shivam714bhandari@gmail.com>
2024-10-22 17:06:57 +0530

    final

--- src/Components/About/NewAbout.jsx
+++ src/Components/About/NewAbout.jsx
@@ -228,7 +228,7 @@ function About() {
             </div>
 
             {/* Responsive button */}
-            <button className="mt-4 bg-blue-500 text-white px-4 py-2 md:px-6 md:py-3 rounded-lg md:mb-4 hover:bg-blue-600">
+            <button className="mt-4 bg-blue-500 text-white px-4 py-2 md:px-6 md:py-3 rounded-lg md:mb-6 hover:bg-blue-600">
               More About &rarr;
             </button>
           </div>
